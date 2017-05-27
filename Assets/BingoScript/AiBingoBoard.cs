ce8e57872e8f4213bc4940351c85e43eb86549b8
F1026120 <mays2288@gmail.com>
2017-05-27 22:29:30 +0800

    FirstPush
    以後在這更新

---
+++ Assets/BingoScript/AiBingoBoard.cs
@@ -0,0 +1,270 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 賓果Ai
+namespace AiBingoBoard
+{
+
+    public class AiStrategy : BingoBoard
+    {
+        public AiStrategy(int b) : base(b)
+        {
+        }
+
+        public int GetNextNumber(AiLevelinterface AiContext)
+        {
+            return AiContext.GetNextNumber(m_Board);
+        }
+
+    }
+
+    public interface AiLevelinterface
+    {
+
+        int GetNextNumber(int[,] m_Board);
+
+    }
+
+    public class AiLevel1 : AiLevelinterface
+    {
+        protected int[] m_LinePoint = new int[12];
+
+
+        public int GetNextNumber(int[,] m_Board) //老師的AI
+        {
+
+            int lineindex = 0;
+            int point = 0;
+
+            //計算列值
+            for (int i = 0; i < 5; i++)
+            {
+                point = 0;
+                for (int j = 0; j < 5; j++)
+                    if (m_Board[i, j] > 0)
+                        point++;
+                m_LinePoint[lineindex++] = point;
+            }
+
+            //計算行值
+            for (int j = 0; j < 5; j++)
+            {
+                point = 0;
+                for (int i = 0; i < 5; i++)
+                    if (m_Board[i, j] > 0)
+                        point++;
+                m_LinePoint[lineindex++] = point;
+            }
+
+            // TODO:左斜,右斜
+            for (int i = 0; i < 5; i++)
+            {
+                point = 0;
+                if (m_Board[i, i] > 0)
+                    point++;
+            }
+            m_LinePoint[lineindex++] = point;
+
+
+            for (int i = 4; i >= 0; i--)
+            {
+                point = 0;
+                if (m_Board[i, 4 - i] > 0)
+                    point++;
+            }
+            m_LinePoint[lineindex++] = point;
+
+            // 取得最少分數者
+            int MinNum = 6;
+            int MinIndex = -1;
+            for (lineindex = 0; lineindex < 12; ++lineindex)
+                if (m_LinePoint[lineindex] != 0 && m_LinePoint[lineindex] < MinNum)
+                {
+                    MinNum = m_LinePoint[lineindex];
+                    MinIndex = lineindex;
+                }
+
+            // 決定出牌,列方向
+            int NextNumber = 0;
+            if (MinIndex < 5)
+            {
+                // 第一個號碼
+                for (int j = 0; j < 5; j++)
+                    if (m_Board[MinIndex, j] != 0)
+                    {
+                        NextNumber = m_Board[MinIndex, j];
+                        break;
+                    }
+            }
+            // 決定出牌,行方向
+            else if (MinIndex < 10)
+            {
+                // 第一個號碼
+                for (int i = 0; i < 5; i++)
+                    if (m_Board[i, MinIndex - 5] != 0)
+                    {
+                        NextNumber = m_Board[i, MinIndex - 5];
+                        break;
+                    }
+            }
+            // TODO,左斜(左上->右下)
+            else if (MinIndex == 10)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    if (m_Board[i, i] != 0)
+                    {
+                        NextNumber = m_Board[i, i];
+                        //Debug.Log("電腦選擇左斜");
+                        break;
+                    }
+                }
+            }
+            // TODO,右斜(右上->左下)
+            else
+            {
+                for (int i = 4; i >= 0; i--)
+                    if (m_Board[i, 4 - i] != 0)
+                    {
+                        NextNumber = m_Board[i, 4 - i];
+                        //Debug.Log("電腦選擇右斜");
+                        break;
+                    }
+            }
+
+            //Debug.Log("電腦出牌[" + NextNumber + "]" + "價值 " + MaxPrice);
+            return NextNumber;
+        }
+        // 決定出牌
+    }
+    public class AiLevel2 : AiLevelinterface
+    {
+        int bound = 5; //正方形邊長
+        public AiLevel2(int b)
+        {
+            bound = b;
+        }
+        public int GetNextNumber(int[,] m_Board)// Ai lebel 2
+        {
+
+            int[,] point = new int[5, 5];
+            int NextNumber = -1;
+            int col = 5;
+            int row = 5;
+            
+                           //計算每個位置價值(point)
+            for (int c = 0; c < bound; c++)
+            {
+                for (int r = 0; r < bound; r++)
+                {
+                    point[c, r] = 0;
+                    if (m_Board[c, r] == 0)//如果以選過則不用判斷
+                        continue;
+                    for (int k = 0; k < bound; k++)
+                    {
+                        if (m_Board[c, k] == 0) point[c, r] += 1;//判斷row方向之已選過的點 
+                        if (m_Board[k, r] == 0) point[c, r] += 1;//判斷col方向之已選過的點
+                        if (c == r || (c + r) == (bound - 1))//如果為斜線上的點
+                        {
+                            if (c == r)//左上向右下的斜線
+                                if (m_Board[k, k] == 0) point[c, r] += 2;
+                            if ((c + r) == (bound - 1))//左下向 右上的斜線
+                                if (m_Board[k, (bound - 1) - k] == 0) point[c, r] += 2;
+
+                        }
+                    }
+                    if (c == r || (c + r) == (bound - 1)) point[c, r] += 8;//斜線上的點加權8
+                    if (c == r && (c + r) == (bound - 1)) point[c, r] += 4;//中心交點 額外加權 4
+
+                }
+            }
+            //找出最有價值的選擇
+            int MaxPrice = -1;
+            int MaxPriceNumber = -1;
+            for (int c = 0; c < 5; c++)
+            {
+                for (int r = 0; r < 5; r++)
+                {
+                    if (point[c, r] > MaxPrice)
+                    {
+                        MaxPrice = point[c, r];
+                        MaxPriceNumber = m_Board[c, r];
+                    }
+                }
+            }
+            NextNumber = MaxPriceNumber;
+            //Debug.Log("電腦出牌[" + NextNumber + "]" + "價值 " + MaxPrice);
+            return NextNumber;
+        }
+    }
+    public class AiLevel3 : AiLevelinterface
+    {
+        public int GetNextNumber(int[,] m_Board) //Ai level 3
+        {
+            int[,] point = new int[5, 5];
+            int NextNumber = -1;
+            int[,] colPrice = new int[5, 5];
+            int[,] rowPrice = new int[5, 5];
+            int bound = 5; //正方形邊長
+                           //計算每個位置價值(point)
+            for (int c = 0; c < bound; c++)
+            {
+                for (int r = 0; r < bound; r++)
+                {
+                    point[c, r] = 0;
+                    colPrice[c, r] = 0;
+                    rowPrice[c, r] = 0;
+                    if (m_Board[c, r] == 0)//如果以選過則不用判斷
+                        continue;
+                    for (int k = 0; k < bound; k++)
+                    {
+                        if (m_Board[c, k] == 0) { point[c, r] += 1; rowPrice[c, r] += 1; }//判斷row方向之已選過的點 
+                        if (m_Board[k, r] == 0) { point[c, r] += 1; colPrice[c, r] += 1; }//判斷col方向之已選過的點
+                        if (c == r || (c + r) == (bound - 1))//如果為斜線上的點
+                        {
+                            if (c == r)//左上向右下的斜線
+                                if (m_Board[k, k] == 0) point[c, r] += 2;
+                            if ((c + r) == (bound - 1))//左下向 右上的斜線
+                                if (m_Board[k, (bound - 1) - k] == 0) point[c, r] += 2;
+
+                        }
+                    }
+                    if (c == r || (c + r) == (bound - 1)) point[c, r] += 8;//斜線上的點加權8
+                    if (c == r && (c + r) == (bound - 1)) point[c, r] += 4;//中心交點 額外加權 4
+                }
+            }
+            //找出最有價值的選擇
+            int MaxPrice = -1;
+            int MaxPriceNumber = -1;
+            int MaxPry = -1;//優先值 => (rowPrice - colPrice )^2
+            for (int c = 0; c < 5; c++)
+            {
+                for (int r = 0; r < 5; r++)
+                {
+                    if (point[c, r] == MaxPrice)//if 兩點價值相等 比較優先度
+                    {
+                        int thepry = (colPrice[c, r] - rowPrice[c, r]) * (colPrice[c, r] - rowPrice[c, r]);//平方取正
+                        if (thepry > MaxPry)
+                        {
+                            MaxPry = thepry;
+                            MaxPrice = point[c, r];
+                            MaxPriceNumber = m_Board[c, r];
+                        }
+
+                    }
+                    else if (point[c, r] > MaxPrice)//比較價值 
+                    {
+                        MaxPrice = point[c, r];
+                        MaxPry = (colPrice[c, r] - rowPrice[c, r]) * (colPrice[c, r] - rowPrice[c, r]);
+                        MaxPriceNumber = m_Board[c, r];
+                    }
+                }
+            }
+            NextNumber = MaxPriceNumber;
+            //Debug.Log("電腦出牌[" + NextNumber + "]" + "價值 " + MaxPrice + "優先值 " + MaxPry);
+            return NextNumber;
+        }
+    }
+}
+
---
+++ Assets/BingoScript/BingoController.cs
@@ -0,0 +1,339 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class BingoController : MonoBehaviour
+{
+    static int bound = 5;
+    static int Wins_Player = 0;
+    static int Wins_Computer = 0;
+    static int PlayerLine = 0;
+    static int ComLine = 0;
+    // 資料結構
+    BingoBoard m_PlayerBoard = new BingoBoard(bound);
+    AiBingoBoard.AiStrategy m_ComBoard = new AiBingoBoard.AiStrategy(bound);
+    // 換誰出手
+    enum WhichOne
+    {
+        Player = 0,
+        Ai,
+        GameOver,
+    }
+    WhichOne m_WhichOnePlay = WhichOne.Player;
+    bool m_bNeedFlush = false;
+
+    // 顯示相關
+    GameObject[,] m_ComGrid;    // 電腦使用的Bingo盤
+    GameObject[,] m_PlayerGrid;    // 玩家使用的Bingo盤
+    Text m_PlayerLine;
+    Text m_ComLine;
+
+
+    // 開始時
+    void Start()
+    {
+        InitComGrid();
+        InitPlayGrid();
+        CreatNextGameButton();
+        GameObject tmpObj = null;
+        tmpObj = GameObject.Find("PlayerLineTxt");
+        m_PlayerLine = tmpObj.GetComponent<Text>();
+        tmpObj = GameObject.Find("ComLineTxt");
+        m_ComLine = tmpObj.GetComponent<Text>();
+
+        m_PlayerBoard.InitBoard();
+        m_ComBoard.InitBoard();
+        m_bNeedFlush = true;
+
+    }
+
+    // GameLoop
+    void Update()
+    {
+        // 換AI出手
+        if (m_WhichOnePlay == WhichOne.Ai)
+        {
+            int NextNumber = m_ComBoard.GetNextNumber(new AiBingoBoard.AiLevel2(bound));
+            ChangeButtonColor(NextNumber);
+            m_ComBoard.SetNumber(NextNumber);
+            m_PlayerBoard.SetNumber(NextNumber);
+            m_bNeedFlush = true;
+            m_WhichOnePlay = WhichOne.Player;
+        }
+
+        // 顯示雙方賓果盤
+        if (m_bNeedFlush)
+        {
+            // 計算雙方分數及顯示
+
+            CalculationDisplay();
+
+            // 判斷勝利
+            if (PlayerLine >= 5 && ComLine < 5)
+            {
+                Wins_Player += 1;
+                CalculationDisplay();
+                m_PlayerLine.text += "你勝了!!!";
+                m_WhichOnePlay = WhichOne.GameOver;
+            }
+
+            if (ComLine >= 5 && PlayerLine < 5)
+            {
+                Wins_Computer += 1;
+                CalculationDisplay();
+                m_ComLine.text += "電腦勝了!!!";
+
+
+                m_WhichOnePlay = WhichOne.GameOver;
+            }
+
+            if (PlayerLine >= 5 && ComLine >= 5)
+            {
+                CalculationDisplay();
+                m_PlayerLine.text += "平手!!!";
+                m_ComLine.text += "平手了!!!";
+                m_WhichOnePlay = WhichOne.GameOver;
+            }
+
+            // 顯示Board內容
+            ShowPlayerBingoBoard();
+            if (m_WhichOnePlay == WhichOne.GameOver)
+                ShowComBingoBoard(false);
+            else
+                ShowComBingoBoard(true);
+            m_bNeedFlush = false;
+        }
+    }
+
+    // 顯示玩家的賓果盤
+    void ShowPlayerBingoBoard()
+    {
+        for (int i = 0; i < 5; ++i)
+            for (int j = 0; j < 5; ++j)
+            {
+                Text theText = m_PlayerGrid[i, j].GetComponentInChildren<Text>();
+                theText.text = string.Format("{0}", m_PlayerBoard.m_Board[i, j]);
+            }
+    }
+
+    // 顯示電腦的賓果盤
+    void ShowComBingoBoard(bool bStartMode)
+    {
+        for (int i = 0; i < 5; ++i)
+            for (int j = 0; j < 5; ++j)
+            {
+                Text theText = m_ComGrid[i, j].GetComponentInChildren<Text>();
+
+                if (!(bStartMode))
+                {
+                    theText.text = "";
+                    if (m_ComBoard.m_Board[i, j] > 0)
+                        theText.text = "*";
+                }
+                else
+                    theText.text = string.Format("{0}", m_ComBoard.m_Board[i, j]);
+            }
+    }
+
+    // 產生電腦使用的Bingo盤
+    void InitComGrid()
+    {
+        m_ComGrid = new GameObject[5, 5];
+        GameObject Obj = GameObject.Find("ComBtn"); // 參考的按鈕
+
+        // 取得按鈕的長寬 
+        RectTransform RectInfo = Obj.GetComponent<RectTransform>();
+        float BtnWidth = RectInfo.rect.width;
+        float BtnHeight = RectInfo.rect.height;
+        // 取得位置
+        Vector3 PosInfo = Obj.transform.position;
+        int GridCount = 0;
+        for (int i = 0; i < 5; ++i)
+            for (int j = 0; j < 5; ++j)
+            {
+                GameObject NewObj = null;
+                if (i == 0 && j == 0)
+                    NewObj = Obj;
+                else
+                    NewObj = GameObject.Instantiate(Obj);
+
+                // 設定位置                
+                m_ComGrid[i, j] = NewObj;
+                NewObj.name = String.Format("{0}{1}", i, j);
+                NewObj.transform.SetParent(Obj.transform.parent);
+
+                // 設定位置
+                float Posx = PosInfo.x + (BtnWidth * j);
+                float Posy = PosInfo.y + -(BtnHeight * i);
+                NewObj.transform.position = new Vector3(Posx, Posy, 0);
+            }
+    }
+
+    // 產生玩家使用的Bingo盤
+    void InitPlayGrid()
+    {
+        m_PlayerGrid = new GameObject[5, 5];
+        GameObject Obj = GameObject.Find("PlayerBtn"); // 參考的按鈕
+
+        // 取得按鈕的長寬 
+        RectTransform RectInfo = Obj.GetComponent<RectTransform>();
+        float BtnWidth = RectInfo.rect.width;
+        float BtnHeight = RectInfo.rect.height;
+        // 取得位置
+        Vector3 PosInfo = Obj.transform.position;
+        int GridCount = 0;
+        for (int i = 0; i < 5; ++i)
+            for (int j = 0; j < 5; ++j)
+            {
+                GameObject NewObj = null;
+                if (i == 0 && j == 0)
+                    NewObj = Obj;
+                else
+                    NewObj = GameObject.Instantiate(Obj);
+
+                // 設定Text                
+                m_PlayerGrid[i, j] = NewObj;
+                NewObj.name = String.Format("{0}{1}", i, j);
+                NewObj.transform.SetParent(Obj.transform.parent);
+
+                // 設定位置
+                float Posx = PosInfo.x + (BtnWidth * j);
+                float Posy = PosInfo.y + -(BtnHeight * i);
+                NewObj.transform.position = new Vector3(Posx, Posy, 0);
+
+                // 設定Button事件
+                Button NewButton = NewObj.GetComponent<Button>();
+                NewButton.onClick.AddListener(() => OnPlayerBtnClick(NewButton));
+            }
+    }
+
+    // 玩家按下Btn
+    public void OnPlayerBtnClick(Button theButton)
+    {
+        if (m_WhichOnePlay != WhichOne.Player)
+            return;
+        //Debug.Log("OnPlayerBtnClick:" + theButton.gameObject.name);
+
+        // 取得按鈕上的值
+        Text theText = theButton.GetComponentInChildren<Text>();
+
+        // 轉換成數字
+        int Number = Int32.Parse(theText.text);
+        if (Number > 0)
+        {
+            ColorBlock thecolors;
+            thecolors = theButton.colors;
+            thecolors.highlightedColor = Color.green;
+            theButton.colors = thecolors;
+
+            ChangeButtonColor(Number);
+            m_PlayerBoard.SetNumber(Number); // 設定為0
+            m_ComBoard.SetNumber(Number);
+
+            m_bNeedFlush = true;
+            m_WhichOnePlay = WhichOne.Ai;
+        }
+    }
+    public void ChangeButtonColor(int Number)
+    {
+        ColorBlock thecolors;
+        GameObject BtnObj;
+        Button theButton;
+
+        for (int c = 0; c < 5; c++)
+            for (int r = 0; r < 5; r++)
+            {
+                if (m_PlayerBoard.m_Board[c, r] == Number)
+                {
+                    BtnObj = m_PlayerGrid[c, r];
+                    theButton = BtnObj.GetComponent<Button>();
+                    thecolors = BtnObj.GetComponent<Button>().colors;
+                    thecolors.normalColor = Color.red;
+                    theButton.colors = thecolors;
+                }
+                if (m_ComBoard.m_Board[c, r] == Number)
+                {
+                    BtnObj = m_ComGrid[c, r];
+                    theButton = BtnObj.GetComponent<Button>();
+                    thecolors = BtnObj.GetComponent<Button>().colors;
+                    thecolors.normalColor = Color.red;
+                    theButton.colors = thecolors;
+                }
+
+            }
+    }
+    public void ResetButtonColor()
+    {
+        ColorBlock thecolors;
+        GameObject BtnObj;
+        Button theButton;
+
+        for (int c = 0; c < 5; c++)
+            for (int r = 0; r < 5; r++)
+            {
+
+                BtnObj = m_PlayerGrid[c, r];
+                theButton = BtnObj.GetComponent<Button>();
+                thecolors = BtnObj.GetComponent<Button>().colors;
+                thecolors.normalColor = Color.white;
+                theButton.colors = thecolors;
+                BtnObj = m_ComGrid[c, r];
+                theButton = BtnObj.GetComponent<Button>();
+                thecolors = BtnObj.GetComponent<Button>().colors;
+                thecolors.normalColor = Color.white;
+                theButton.colors = thecolors;
+            }
+
+    }
+
+    public void NextGame()//重新下一局
+    {
+        ResetButtonColor();
+        m_PlayerBoard.InitBoard();
+        m_ComBoard.InitBoard();
+        m_bNeedFlush = true;
+        m_WhichOnePlay = WhichOne.Player;
+    }
+    public void CreatNextGameButton()
+    {
+        /*
+        GameObject Btn_Restart;
+        GameObject _text;
+        Btn_Restart = new GameObject();
+        _text = new GameObject();
+        // 取得按鈕的長寬 
+        RectTransform RectInfo = Btn_Restart.AddComponent<RectTransform>();
+        float BtnWidth = 160f;
+        float BtnHeight = 30f;
+        // 設定Text                
+        Btn_Restart.name = "BtnRestart";
+        Text Text = _text.AddComponent<Text>();
+        _text.transform.parent = Btn_Restart.transform;
+        Text.text = "開始下一局";
+        //NewObj.transform.SetParent(Obj.transform.parent);
+        Btn_Restart.AddComponent<Image>();
+        // 設定位置
+        float Posx = 290f;
+        float Posy = 166f;
+        Btn_Restart.transform.position = new Vector3(Posx, Posy, 0);
+
+        // 設定Button事件
+        Button NewButton = Btn_Restart.AddComponent<Button>();
+        NewButton.onClick.AddListener(() => NextGame());
+        */
+        GameObject Btn_NextGame = GameObject.Find("BtnNextGame");
+        Button theButton = Btn_NextGame.GetComponent<Button>();
+        theButton.onClick.AddListener(() => NextGame());
+    }
+    public void CalculationDisplay()
+    {
+        // 計算雙方分數及顯示
+        PlayerLine = m_PlayerBoard.CountLine();
+        m_PlayerLine.text = string.Format("目前連線數:{0}\n勝場數:{1}", PlayerLine, Wins_Player);
+        ComLine = m_ComBoard.CountLine();
+        m_ComLine.text = string.Format("目前連線數:{0}\n勝場數:{1}", ComLine, Wins_Computer);
+    }
+
+}
